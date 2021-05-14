136fb3fdea24c3520cbdd7872fb4bc470b517a43
AakashCOMP229 <a563@my.centennialcollege.ca>
2021-05-14 23:25:19 +0530

    initial commit

---
+++ Scripts/app.js
@@ -0,0 +1,10 @@
+"use strict";
+(function()
+{
+    function Start()
+    {
+        let myVariable = 10;
+        console.log("my number is"+ myVariable);
+    }
+    window.addEventListener("load", Start);
+})
